81e477c757a9a5b397301368a50e261dbfa88978
Theraphia Li (李思颀) <54740400+Theraphia@users.noreply.github.com>
2021-07-12 14:14:08 +0800

    Update views.py

--- mainpage/views.py
+++ mainpage/views.py
@@ -168,7 +168,6 @@ def show_user_comment(request):
     包括被评论文章的id, 评论的id，标题title，时间time，类别1article_type1, 类别2article_type2，类别3article_type3
     评论时间time, 评论的内容
     :param request:
-
     message {
         comment {
             commentID.....
@@ -201,8 +200,8 @@ def show_user_comment(request):
         temp = dict()
         temp['comment'] = temp_c
         temp['article'] = temp_a
-        comments_data.append(temp)
-        return JsonResponse(data=comments_data, safe=False)
+        comments_data.append(temp)    
+    return JsonResponse(data=comments_data, safe=False)
 
 
 def edit_comment(request):
